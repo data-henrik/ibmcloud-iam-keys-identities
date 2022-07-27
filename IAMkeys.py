786b5a21df38ab8dcd1dbb9a9e71b9698c5ebedd
Henrik Loeser <hloeser@de.ibm.com>
2022-07-27 14:31:37 +0200

    first drop

---
+++ IAMkeys.py
@@ -0,0 +1,154 @@
+import requests, json, sys,os, base64
+from urllib.parse import urlparse,parse_qs
+
+iamInfoUrl="https://iam.cloud.ibm.com/identity/.well-known/openid-configuration"
+
+
+def extractNextPageToken(next_url):
+    o=urlparse(next_url)
+    q=parse_qs(o.query)
+    return q['pagetoken'][0]
+
+
+def readApiKey(filename):
+    with open(filename) as data_file:
+        credentials = json.load(data_file)
+    api_key = credentials.get('apikey')
+    return api_key
+
+
+def getAuthTokens(api_key):
+    url     = "https://iam.cloud.ibm.com/identity/token"
+    headers = { "Content-Type" : "application/x-www-form-urlencoded" }
+    data    = "apikey=" + api_key + "&grant_type=urn:ibm:params:oauth:grant-type:apikey"
+    response  = requests.post( url, headers=headers, data=data )
+    return response.json()
+
+def getIAMDetails(api_key, iam_token):
+    url     = "https://iam.cloud.ibm.com/v1/apikeys/details"
+    headers = { "Authorization" : iam_token, "IAM-Apikey" : api_key, "Content-Type" : "application/json" }
+    response  = requests.get( url, headers=headers )
+    return response.json()
+
+
+def getAccounts(iam_token):
+    url     = "https://accounts.cloud.ibm.com/v1/accounts"
+    headers = { "Authorization" : iam_token }
+    response  = requests.get( url, headers=headers )
+    return response.json()
+
+
+def getApiKeys(iam_token, account_id, iam_id):
+    pagesize=100
+    url = 'https://iam.cloud.ibm.com/v1/apikeys'
+    headers = { "Authorization" : iam_token }
+    payload = {"account_id": account_id, "iam_id": iam_id, "pagesize":pagesize}
+    response = requests.get(url, headers=headers, params=payload)
+    result=response.json()
+    temp=result
+    while 'next' in temp:
+        payload = {"account_id": account_id, "iam_id": iam_id, "pagesize":pagesize, "pagetoken":extractNextPageToken(temp['next'])}
+        response=requests.get(url, headers=headers, params=payload)
+        temp=response.json()
+        result['apikeys'].extend(temp['apikeys'])
+    return result
+
+def getApiKeyDetails(iam_token, apikey_id):
+    url = 'https://iam.cloud.ibm.com/v1/apikeys/{}'.format(apikey_id)
+    headers = { "Authorization" : iam_token }
+    # we want EVERYTHING
+    payload={"include_history":True, "include_activity":True}
+    response = requests.get(url, headers=headers, params=payload)
+    return response.json()
+
+
+def getServiceIDs(iam_token, account_id):
+    pagesize=5
+    url = 'https://iam.cloud.ibm.com/v1/serviceids'
+    headers = { "Authorization" : iam_token }
+    payload = {"account_id": account_id, "pagesize": pagesize}
+    response = requests.get(url, headers=headers, params=payload)
+    result=response.json()
+    temp=result
+    while 'next' in temp:
+        payload = {"account_id": account_id, "pagesize":pagesize, "pagetoken":extractNextPageToken(temp['next'])}
+        response=requests.get(url, headers=headers, params=payload)
+        temp=response.json()
+        result['serviceids'].extend(temp['serviceids'])
+    return result
+
+# loop over all the API keys for the (user / service) ID and retrieve the details
+def getAndPrintAPIKeys(iam_token, account_id, iam_id):
+    api_key_list=getApiKeys(iam_token, account_id, iam_id)
+    for apikey in api_key_list['apikeys']:
+        print(apikey)
+        apikey_details=getApiKeyDetails(iam_token, apikey['id'])
+        # some tricky printing because "activity" and last_authn might not be present
+        print("{},{},{},{},{},{},{}".format(apikey_details['iam_id'],apikey_details['created_by'],
+                                            apikey_details['created_at'],apikey_details['name'],apikey_details['id'],
+                                            apikey_details.get('activity',{}).get('last_authn',None),
+                                            apikey_details.get('activity',{}).get('authn_count', None)))
+
+# retrieve details on API keys for the current user and the related service IDs
+def getEverything(iam_token,account_id, iam_id):
+    print('iam_id, created_by, created_at, name, id, last_authn, authn_count')
+    print('=================================================================')
+    getAndPrintAPIKeys(iam_token, account_id, iam_id)
+    serviceids=getServiceIDs(iam_token, account_id)
+    for serviceid in serviceids['serviceids']:
+        print()
+        getAndPrintAPIKeys(iam_token, account_id, serviceid['iam_id'])
+
+# use split and base64 to get to the content of the IAM token
+def extractAccount(iam_token):
+    data = iam_token.split('.')
+    padded = data[1] + "="*divmod(len(data[1]),4)[1]
+    jsondata = json.loads(base64.urlsafe_b64decode(padded))
+    return jsondata
+
+
+# some help
+def printHelp(progname):
+    print ("Usage: "+progname+" [credential-file]")
+
+if __name__== "__main__":
+    credfile=None
+    iam_token=None
+    iam_id=None
+    account_id=None
+
+    if (len(sys.argv)<2):
+        if 'IBMCLOUD_TOKEN' in os.environ:
+            iam_token=os.getenv('IBMCLOUD_TOKEN')
+            token_data=extractAccount(iam_token)
+            account_id=token_data["account"]["bss"]
+            iam_id=token_data['iam_id']
+        else:
+            printHelp(sys.argv[0])
+            exit()
+    elif (len(sys.argv)==2):
+        credfile=sys.argv[1]
+        mode=1
+    else:
+        print ("unknown options")
+        printHelp(sys.argv[0])
+        exit()
+
+    if iam_token is None:
+        print ("Reading credentials")
+        apiKey=readApiKey(credfile)
+        print ("generating auth tokens")
+        authTokens=getAuthTokens(api_key=apiKey)
+        #print ("authTokens:")
+        print (json.dumps(authTokens, indent=2))
+        iam_token='Bearer '+authTokens["access_token"]   
+
+
+        print ("Getting account details")
+        accDetails=getIAMDetails(api_key=apiKey, iam_token=iam_token)
+        account_id=accDetails['account_id']
+        iam_id=accDetails['iam_id']
+  
+    
+    
+    getEverything(iam_token, account_id, iam_id)
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# ibmcloud-iam-keys-identities
+# Information about inactive identities and API keys in your IBM Cloud account
 Retrieve information about IBM Cloud IAM keys and identify inactive identities
+
+
+### Preparation
+All commands and scripts can be run in IBM Cloud Shell. The following steps are needed to get started.
+1. Visit https://cloud.ibm.com, if not done log in, then open the [IBM Cloud Shell](https://cloud.ibm.com/shell).
+2. In the new terminal, set an environment variable with the access token for you session:
+   ```sh
+   export IBMCLOUD_TOKEN=$(ibmcloud iam oauth-tokens --output json | jq -r '.iam_token')
+   ```
+3. Set an environment variable with the account ID:
+   ```
+   export IBMCLOUD_ACCOUNTID=$(ibmcloud account show --output json | jq -r '.account_id')
+   ```
+
+
+### Use curl to trigger and retrieve report on inactive identities
+With the above preparations, you can use the command line investigate inactive identities in your IBM Cloud account.
+
+1. Trigger a new report about inactive identities. The result is the reference to the new report.
+   ```
+   curl -X POST "https://iam.cloud.ibm.com/v1/activity/accounts/${IBMCLOUD_ACCOUNTID}/report" -H "Authorization: ${IBMCLOUD_TOKEN}" -H 'Content-Type: application/json' 
+   ```
+   By default, the duration is 720 hours which is 30 days. You can change the duration by passing in an additional parameter. Adapt it to your preferences (shown for 90 days):
+   ```
+   curl -X POST "https://iam.cloud.ibm.com/v1/activity/accounts/${IBMCLOUD_ACCOUNTID}/report?duration=2160" -H "Authorization: ${IBMCLOUD_TOKEN}" -H 'Content-Type: application/json' 
+   ```
+2. A some seconds to minute, you can retrieve the report. Using **latest** as reference, the latest available report is returned. Replace **latest** with the reference from the previous command to retrieve that specific report.
+   ```
+   curl -s -X GET "https://iam.cloud.ibm.com/v1/activity/accounts/${IBMCLOUD_ACCOUNTID}/report/latest" -H "Authorization: ${IBMCLOUD_TOKEN}" -H 'Content-Type: application/json' | jq
+   ```
+
+### Use Python to investigate your API keys and service IDs
+
+1. Download and save the Python script:
+   ```
+   curl -s https://raw.githubusercontent.com/data-henrik/ibmcloud-iam-keys-identities/main/IAMkeys.py
+   ```
+
+2. Run the Python script:
+   ```
+   python3 IAMkeys.ps
+   ```
+
+
+## License
+See the [LICENSE](LICENSE) file.
